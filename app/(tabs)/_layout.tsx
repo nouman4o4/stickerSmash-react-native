ed45116ef22ba5c4df8fd79a39cb3deca7f661fe
Nouman Khan <nomiofficial68@gmail.com>
2026-06-21 23:54:04 +0500

    feat: create ImageViewer.tsx component and Button.tsx and use them tabs/index.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,63 @@
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5"
+import Ionicons from "@expo/vector-icons/Ionicons"
 import { Tabs } from "expo-router"
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="about" options={{ title: "About" }} />
-      <Tabs.Screen name="nomi" options={{ title: "Nomi" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={
+                focused ? "information-circle" : "information-circle-outline"
+              }
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="nomi"
+        options={{
+          title: "Nomi",
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome5
+              name={focused ? "user-alt" : "user"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { Link } from "expo-router"
-import { StyleSheet, Text, View } from "react-native"
+import Button from "@/components/Button"
+import ImageViewer from "@/components/ImageViewer"
+import { StyleSheet, View } from "react-native"
+
+const PlaceholderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/about"} style={styles.button}>
-        About
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photot" />
+      </View>
     </View>
   )
 }
@@ -27,5 +33,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: "underline",
     color: "#fff",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 })
